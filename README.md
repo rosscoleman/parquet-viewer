cd7b434e9e69acec3b9ead55b8d9210f9904f8e8
Ross Coleman <rossco85@gmail.com>
2023-07-14 02:32:09 -0500

    Add File->Open

--- README.md
+++ README.md
@@ -1,12 +1,18 @@
-Some data sets that are small enough to fit on a single workstation.
+Cross-platform viewer for Apache Parquet files using Qt for Python.
 
-Parquet and Feature files are still preferable to CSVs because
-* They are binary formats. Numeric data types are maintained.
-* Data sets may fit on workstation local storage but not in RAM.
+This is currently just a proof of concept.
 
-In these cases a desktop file viewer for Parquet and Feather files would be useful.
-* Windows
-* Mac
-* Linux desktop environments
+This is useful for Parquet files that are small enough to fit on local storage for a workstation or laptop.
 
-Double click a file and view it. Useful for colleagues that don't use Pandas, Dask, etc.
+TODO:
+* Package as stand-alone Windows and Mac executables.
+* Handle Windows and Mac file associations. (Double click file to open.)
+  * KDE plasma?
+  * GNOME3?
+
+Possible enhancements:
+* Read Parquet files in chunks as you scoll, for files that fit on disk but not in RAM.
+  * On the other hand, are these so big that a metadata viewer and summary statistics are all that make sense?
+* Sort by clicking on a column header
+  * Harder if we only load chunks into RAM at a time.
+* Support Apache Feather files
--- parquet_viewer.py
+++ parquet_viewer.py
@@ -1,18 +1,26 @@
 import argparse
 from datetime import datetime
 import sys
-from PySide6 import QtCore, QtGui, QtWidgets
+from PySide6 import QtCore, QtWidgets
 from PySide6.QtCore import Qt
+from PySide6.QtGui import QAction
+from PySide6.QtWidgets import QMenu
 import numpy as np
 import pandas as pd
 
 # https://www.pythonguis.com/tutorials/qtableview-modelviews-numpy-pandas/
 
-class TableModel(QtCore.QAbstractTableModel):
 
+class TableModel(QtCore.QAbstractTableModel):
     def __init__(self, data):
         super(TableModel, self).__init__()
         self._data = data
+
+    def replace_data(self, df):
+        # self.dataChanged.emit(topLeft, bottomRight) was not working
+        self.beginResetModel()
+        self._data = df
+        self.endResetModel()
 
     def data(self, index, role):
         if role == Qt.DisplayRole:
@@ -25,26 +33,30 @@ class TableModel(QtCore.QAbstractTableModel):
 
             if isinstance(value, float) or isinstance(value, np.floating):
                 # Render float to 2 dp
-                return "%.2f" % value
+                return "%.4f" % value
 
             if isinstance(value, str):
                 # Render strings with quotes
                 return '"%s"' % value
-            
+
             return str(value)
-        
+
         if role == Qt.TextAlignmentRole:
             value = self._data.iloc[index.row(), index.column()]
 
-            if isinstance(value, int) or isinstance(value, float) or isinstance(value, np.number):
+            if (
+                isinstance(value, int)
+                or isinstance(value, float)
+                or isinstance(value, np.number)
+            ):
                 # Align right, vertical middle.
                 return Qt.AlignVCenter + Qt.AlignRight
 
     def rowCount(self, index):
-        return self._data.shape[0]
+        return self._data.shape[0] if self._data is not None else 0
 
     def columnCount(self, index):
-        return self._data.shape[1]
+        return self._data.shape[1] if self._data is not None else 0
 
     def headerData(self, section, orientation, role):
         # section is the index of the column/row.
@@ -57,8 +69,7 @@ class TableModel(QtCore.QAbstractTableModel):
 
 
 class MainWindow(QtWidgets.QMainWindow):
-
-    def __init__(self, data_frame):
+    def __init__(self, data_frame=None):
         super().__init__()
 
         self.table = QtWidgets.QTableView()
@@ -66,25 +77,64 @@ class MainWindow(QtWidgets.QMainWindow):
         self.model = TableModel(data_frame)
         self.table.setModel(self.model)
 
+        self._createActions()
+        self._connectActions()
+        self._createMenuBar()
+
         self.setCentralWidget(self.table)
+
+    def _createActions(self):
+        # Creating action using the first constructor
+        self.newAction = QAction(self)
+        self.openAction = QAction("&Open...", self)
+        self.exitAction = QAction("&Exit", self)
+
+    def _connectActions(self):
+        self.openAction.triggered.connect(self.openFile)
+        self.exitAction.triggered.connect(self.close)
+
+    def _createMenuBar(self):
+        menuBar = self.menuBar()
+        # Creating menus using a QMenu object
+        fileMenu = QMenu("&File", self)
+        fileMenu.addAction(self.openAction)
+        fileMenu.addAction(self.exitAction)
+        menuBar.addMenu(fileMenu)
+
+    def openFile(self):
+        (fileName, selectedFilter) = QtWidgets.QFileDialog.getOpenFileName(
+            self, "Open Parquet File", "", "Parquet Files (*.parquet)"
+        )
+
+        print(fileName)
+        df = pd.read_parquet(fileName, dtype_backend="pyarrow")
+        self.model.replace_data(df)
 
 
 def main():
     parser = argparse.ArgumentParser(
-                    prog='parquet_viewer',
-                    description='Display an Apache Parquet File',
-                    )
-    parser.add_argument("filepath")
+        prog="parquet_viewer",
+        description="Display an Apache Parquet File",
+    )
+    parser.add_argument("-f", "--file", required=False)
 
     args = parser.parse_args()
 
-    df = pd.read_parquet(args.filepath)
-    print(df.shape)
+    app = QtWidgets.QApplication(sys.argv)
 
-    app=QtWidgets.QApplication(sys.argv)
-    window=MainWindow(df)
+    if args.file:
+        df = pd.read_parquet(args.file, dtype_backend="pyarrow")
+
+        print(df.shape)
+        window = MainWindow(df)
+    else:
+        window = MainWindow()
+        window.openFile()
+
+    window.resize(800, 600)
     window.show()
     app.exec()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
